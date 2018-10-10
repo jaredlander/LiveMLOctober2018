849e0b5f8d705d6422461c5c88833f0c31b9a3b9
Jared Lander <info@jaredlander.com>
2018-10-10 13:52:29 -0400

    Showed first glmnet

--- code/glmnet.r
+++ code/glmnet.r
@@ -15,3 +15,24 @@ valueFormula <- TotalValue ~ FireService +
     LotFront + LotDepth + BldgFront + 
     BldgDepth + LotType + Landmark + BuiltFAR +
     Built + HistoricDistrict - 1
+valueFormula
+class(valueFormula)
+
+value1 <- lm(valueFormula, data=land_train)
+summary(value1)
+coefplot(value1, sort='magnitude')
+
+landX_train <- build.x(valueFormula, data=land_train, 
+                       contrasts=FALSE, sparse=TRUE)
+landY_train <- build.y(valueFormula, data=land_train)
+
+denseMat <- build.x(valueFormula, data=land_train, 
+                       contrasts=FALSE, sparse=FALSE)
+pryr::object_size(landX_train)
+pryr::object_size(denseMat)
+
+value2 <- glmnet(x=landX_train, y=landY_train, family='gaussian')
+head(coef(value2), n=30)
+View(as.matrix(coef(value2)))
+
+plot(value2, xvar='lambda')
