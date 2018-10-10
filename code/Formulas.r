5e9f10b6516fb7a2b54b7167a42bf970e1120d04
Jared Lander <info@jaredlander.com>
2018-10-10 12:53:31 -0400

    Showed formulas

--- code/Formulas.r
+++ code/Formulas.r
@@ -8,3 +8,23 @@ boros <- tibble::tribble(
     'Bronx', 1455000, 42, 21,
     'Staten Island', 475000, 60, 3
 )
+
+boros
+
+y ~ x
+
+build.x( ~ Pop, data=boros)
+build.x( ~ Pop + Size, data=boros)
+build.x( ~ Pop * Size, data=boros)
+1600000*23
+build.x( ~ Pop : Size, data=boros)
+build.x( ~ Pop + Pop:Size, data=boros)
+build.x( ~ Pop * Size * Random, data=boros)
+
+build.x( ~ Pop + Size - 1, data=boros)
+
+build.x( ~ Boro, data=boros)
+build.x( ~ Boro, data=boros, contrasts=FALSE)
+
+build.x( ~ Pop + Size + Boro - 1, data=boros, contrasts=FALSE)
+build.x( ~ Pop + Size + Boro - 1, data=boros, contrasts=FALSE, sparse=TRUE)
