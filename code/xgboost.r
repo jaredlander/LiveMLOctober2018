fb7b37e1725c5efae7bc861208e35e99b73c559b
Jared Lander <info@jaredlander.com>
2018-10-11 12:43:49 -0400

    Built data objects

--- code/xgboost.r
+++ code/xgboost.r
@@ -19,3 +19,19 @@ histFormula <- HistoricDistrict ~ FireService +
     LotFront + LotDepth + BldgFront + 
     BldgDepth + LotType + Landmark + BuiltFAR +
     Built + TotalValue - 1
+
+landX_train <- build.x(histFormula, data=land_train, 
+                       contrasts=FALSE, sparse=TRUE)
+landY_train <- build.y(histFormula, data=land_train) %>% 
+    as.factor() %>% as.integer() - 1
+head(landY_train, n=20)
+
+landX_val <- build.x(histFormula, data=land_val, 
+                       contrasts=FALSE, sparse=TRUE)
+landY_val <- build.y(histFormula, data=land_val) %>% 
+    as.factor() %>% as.integer() - 1
+
+xgTrain <- xgb.DMatrix(data=landX_train, label=landY_train)
+xgTrain
+xgVal <- xgb.DMatrix(data=landX_val, label=landY_val)
+
